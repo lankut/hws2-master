c32d6678e6b9ff445936a92dab7bcbbba078aff8
lankut <lankute@mail.ru>
2025-12-23 18:48:56 +0300

    sprint2_hw2_done_11.0

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,8 +4,47 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
+            sx={{
+                maxWidth: '150px',
+                color: '#00CC22',
+                '& .MuiSlider-thumb': {
+                    backgroundColor: '#00CC22',
+                    // position: 'relative', // важно для позиционирования псевдоэлемента
+                    // zIndex: 3,
+                    '&::after': {
+                        content: '""',
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 18,
+                        height: 18,
+                        borderRadius: '50%',
+                        backgroundColor: '#fff',
+                        display: 'block', // это нужно для отображения псевдоэлемента
+                        // zIndex: 6,
+                    },
+                    // Добавляем внутрь thumb и маленькую точку внутри псевдоэлемента:
+                    // '&::after > span': {
+                    //     content: '""',
+                    //     position: 'absolute',
+                    //     top: '50%',
+                    //     left: '50%',
+                    //     transform: 'translate(-50%, -50%)',
+                    //     width: 6,
+                    //     height: 6,
+                    //     borderRadius: '50%',
+                    //     backgroundColor: '#00CC22',
+                    //     display: 'block',
+                    //     // zIndex: -3,
+                    // },
+                },
+                '& .MuiSlider-track': {
+                    backgroundColor: '#00CC22',
+                },
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#8B8B8B',
+                },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
